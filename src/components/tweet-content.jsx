2ff8c010c8ae6c069eab2918f05563d28d3581f2
djoKadima <djokadimatshibangu@gmail.com>
2023-11-21 20:14:49 +0100

    import TweetActions component in TweetContent

--- src/components/tweet-content.jsx
+++ src/components/tweet-content.jsx
@@ -1,9 +1,11 @@
+import TweetActions from "./tweet-actions"
 import TweetBody from "./tweet-body"
 
 const TweetContent = () =>{
     return (
         <div className="tweet-content">
             <TweetBody/>
+            <TweetActions/>
         </div>
     )
 }
