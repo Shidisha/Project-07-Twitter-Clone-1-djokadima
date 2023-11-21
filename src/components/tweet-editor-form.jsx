d3137f24b2eeceea8d66bee2c63dc15766d6c8dd
djoKadima <djokadimatshibangu@gmail.com>
2023-11-21 18:29:51 +0100

    import a TweetEditorButtons component in tweetEditorForm component

--- src/components/tweet-editor-form.jsx
+++ src/components/tweet-editor-form.jsx
@@ -1,9 +1,11 @@
 import TweetEditorInput from "./tweet-editor-input"
+import TweetEditorButtons from "./tweet-editor-buttons"
 
 const TweetEditorForm = ()=>{
     return(
         <div className="tweet-editor-form">
             <TweetEditorInput/>
+            <TweetEditorButtons/>
         </div>
     )
 }
